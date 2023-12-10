f5f6d06b38d1ecb2273da033f4d372c956d4abc8
z9in <113665653+z9in@users.noreply.github.com>
2023-12-11 07:02:48 +0900

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-    return (
-      <div className='Apps'>
-        <h1 className='h1'>앱점제이에스</h1>
-        <h1 className="h2">h2</h1>
-      </div>
-    )
-  }
--- pages/main/main.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function main() {
-    return(
-        <h1>메인페이지</h1>
-    ) 
-}
